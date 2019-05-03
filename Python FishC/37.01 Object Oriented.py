db481b25ce93ffbf05abf4f05e30623ef70a116b
ConAntares <LukeNiu@outlook.com>
2019-05-04 01:59:58 +0800

    Update

--- Python FishC/37.01 Object Oriented.py
+++ Python FishC/37.01 Object Oriented.py
@@ -57,3 +57,58 @@ a.fun()
     # A
 b.fun()
     # B
+
+## Self in Object
+
+class Ball:
+    # Methods
+    def set_name(self,name):
+        self.name = name
+    def kick(self):
+        print("I'm %s, Damn! Who kicked me?" %self.name)
+
+a = Ball()
+a.set_name("Ball A")
+b = Ball()
+b.set_name("Ball B")
+c = Ball()
+c.set_name("Potato")
+a.kick()
+    # I'm Ball A, Damn! Who kicked me?
+c.kick()
+    # I'm Potato, Damn! Who kicked me?
+
+## __init__
+
+class iBall:
+    def __init__(self,name):
+        self.name = name
+    def kick(self):
+        print("I'm %s, Damn! Who kicked me?" %self.name)
+
+d = iBall("Tomato")
+d.kick()
+    # I'm Tomato, Damn! Who kicked me?
+
+## Public Variable
+
+class Person:
+    name = "FishC"
+
+p = Person()
+print(p.name)
+    # FishC
+
+## Private Variable
+
+class PersonP:
+    __name = "FishC"
+    def get_name(self):
+        return self.__name
+
+p = PersonP()
+print(p.get_name())
+    # FishC
+re = p._PersonP__name
+print(re)
+    # FishC
