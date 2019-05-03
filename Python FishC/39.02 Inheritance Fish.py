25171a1052ba38fdde215032663d006983f93001
ConAntares <LukeNiu@outlook.com>
2019-05-04 02:31:12 +0800

    Update

--- Python FishC/39.02 Inheritance Fish.py
+++ Python FishC/39.02 Inheritance Fish.py
@@ -7,3 +7,37 @@ class Fish:
         self.x = rd.randint(0,10)
         self.y = rd.randint(0,10)
     def move(self):
+        self.x = self.x - 1
+        print("My position is: (%d,%d)." %(self.x,self.y))
+
+class Goldfish(Fish):
+    pass
+
+class Carp(Fish):
+    pass
+
+class Salmon(Fish):
+    pass
+
+class Shark(Fish):
+    def __init__(self):
+        super().__init__()              # or use: Fish.__init__(self)
+        self.hungry = True
+    def eat(self):
+        if self.hungry:
+            print("Eat fish!")
+            self.hungry = False
+        else:
+            print("The party is over.")
+
+fish = Fish()
+fish.move()
+fish.move()
+goldfish = Goldfish()
+goldfish.move()
+goldfish.move()
+shark = Shark()
+shark.eat()
+shark.eat()
+shark.move()
+shark.move()
---
+++ Python FishC/39.03 Multiple Inheritance.py
@@ -0,0 +1,17 @@
+#### Multiple inheritance
+
+class Base1:
+    def Foo1(self):
+        print("Foo1 is under Base1")
+
+class Base2:
+    def Foo2(self):
+        print("Foo2 is under Base2")
+
+class C(Base1,Base2):
+    pass
+
+c = C()
+c.Foo1()
+    # Foo1 is under Base1
+c.Foo2()
