61a713ecac089d5ed03e7691ab9f37b6d8bfe4cf
ConAntares <LukeNiu@outlook.com>
2019-05-04 02:08:22 +0800

    Update

---
+++ Python FishC/39.01 Inheritance.py
@@ -0,0 +1,28 @@
+#### Inheritance
+
+"""
+class Derived_Class_Name(Base_Class_Name):
+    Derived_Class_Name: Sub Class
+    Base_Class_Name: Parent Class
+"""
+
+class Parent:
+    def hello(self):
+        print("Parent Class")
+
+class Child(Parent):
+    pass
+
+p = Parent()
+p.hello()
+    # Parent Class
+c = Child()
+c.hello()
+    # Parent Class
+
+class Sub(Parent):
+    def hello(self):
+        print("Sub Class")
+s = Sub()
+s.hello()\
+    # Sub Class
