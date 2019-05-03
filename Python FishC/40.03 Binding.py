bb6da7d1d1c665b7cda5f75a5e960318314afdde
ConAntares <LukeNiu@outlook.com>
2019-05-04 03:01:25 +0800

    Update

--- Python FishC/40.03 Binding.py
+++ Python FishC/40.03 Binding.py
@@ -19,3 +19,5 @@ exam.set_XY(4,5)
 print(exam.__dict__)            # {'x': 4, 'y': 5}
 print(Bind.__dict__)            # {'__module__': '__main__', 'set_XY': <function Bind.set_XY at 0x000001D49B1152F0>, 'print_XY': <function Bind.print_XY at 0x000001D49B115378>, '__dict__': <attribute '__dict__' of 'Bind' objects>, '__weakref__': <attribute '__weakref__' of 'Bind' objects>, '__doc__': None}
 
+del Bind
+exam.print_XY()                 # 4 5
