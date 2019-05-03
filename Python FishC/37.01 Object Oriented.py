470409e10f98a7372ea376b60998f81e0c4cb3b3
ConAntares <LukeNiu@outlook.com>
2019-05-04 00:09:22 +0800

    Update

---
+++ Python FishC/37.01 Object Oriented.py
@@ -0,0 +1,42 @@
+#### Object Oriented
+
+"""
+{Properties} + {Methods} = Object
+"""
+
+## Class: Encapsulation
+
+class Turtle:
+    # Properties
+    color  = "green"
+    weight = 10
+    legs   = 4
+    shell  = True
+    mouth  = "Big"
+
+    # Methods
+    def climb(self):
+        print("Climb")
+    def run(self):
+        print("Run")
+    def bite(self):
+        print("Bite")
+    def eat(self):
+        print("Eat")
+    def Sleep(self):
+        print("Sleep")
+
+class MyList(list):
+    pass
+    
+## Object:  Inheritance
+
+tt = Turtle()
+tt.eat()
+    # Eat
+
+list = MyList()
+list.append(3.14)
+list.append(6.18)
+list.append(2.71)
+print(list)
--- Python FishC/37.01 Object.py
+++
@@ -1,6 +0,0 @@
-#### Object
-
-"""
-{Properties} + {Methods} = Object
-"""
-
