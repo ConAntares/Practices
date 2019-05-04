3c0946e05658e5df9953371965c0f3146df7dee3
ConAntares <LukeNiu@outlook.com>
2019-05-05 04:36:40 +0800

    Update

---
+++ Python FishC/44.02 List of Magic Methods.py
@@ -0,0 +1,44 @@
+#### List of Magic Methods
+
+## Binary Operators
+"""
+Methods                                         : Operator
+object.__add__(self, other)                     : +
+object.__sub__(self, other)                     : -
+object.__mul__(self, other)                     : *
+object.__floordiv__(self, other)                : //
+object.__truediv__(self, other)                 : /
+object.__mod__(self, other)                     : %
+object.__pow__(self, other[, modulo])           : **
+object.__lshift__(self, other)                  : <<
+object.__rshift__(self, other)                  : >>
+object.__and__(self, other)                     : &
+object.__xor__(self, other)                     : ^
+object.__or__(self, other)                      : |
+"""
+
+## Unary Operators
+"""
+Methods                                         : Operator
+object.__neg__(self)                            : -
+object.__pos__(self)                            : +
+object.__abs__(self)                            : abs()
+object.__invert__(self)                         : ~
+object.__complex__(self)                        : complex()
+object.__int__(self)                            : int()
+object.__long__(self)                           : long()
+object.__float__(self)                          : float()
+object.__oct__(self)                            : oct()
+object.__hex__(self)                            : hex()
+"""
+
+## Comparison Operators
+"""
+Methods                                         : Operator
+object.__lt__(self, other)                      : <
+object.__le__(self, other)                      : <=
+object.__eq__(self, other)                      : ==
+object.__ne__(self, other)                      : !=
+object.__ge__(self, other)                      : >=
+object.__gt__(self, other)                      : >
+"""
