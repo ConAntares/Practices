6952164c94f1593e15913a992bad27a987270916
ConAntares <LukeNiu@outlook.com>
2019-05-06 04:18:09 +0800

    update

--- Python FishC/47.01 Descriptor.py
+++ Python FishC/47.01 Descriptor.py
@@ -9,4 +9,19 @@ __delete__(self, instance)
 
 class MyDescriptor:
     def __get__(self, instance, owner):
-        print("Getting...",self,instance,owner)
+        print("Getting...", self, instance,owner)
+    def __set__(self, instance, value):
+        print("Setting...", self, instance,value)
+    def __delete__(self, instance):
+        print("Deleting...", self, instance)
+
+class Test:
+    x = MyDescriptor()
+
+test = Test()
+test.x          
+    # Getting... <__main__.MyDescriptor object at 0x000001DC8040A780> <__main__.Test object at 0x000001DC8040AE48> <class'__main__.Test'>
+test.x = "X-man"
+    # Setting... <__main__.MyDescriptor object at 0x000001D48EF4AF60> <__main__.Test object at 0x000001D48EF4AF98> X-man
+del test.x
+    # Deleting... <__main__.MyDescriptor object at 0x000001D66C74AF60> <__main__.Test object at 0x000001D66C74AF98>
---
+++ Python FishC/47.03 Temperature.py
@@ -0,0 +1,25 @@
+#### Temperature
+
+class Celsius:
+    def __init__(self,value = 26.0):
+        self.value = float(value)
+    def __get__(self, instance, owner):
+        return self.value
+    def __set__(self, instance, value):
+        self.value = float(value)
+
+class Fahrenheit:
+    def __get__(self, instance, owner):
+        return instance.cel * 1.8 + 32
+    def __set__(self, instance, value):
+        instance.cel = (float(value)-32)/1.8
+
+class Temperature:
+    cel = Celsius()
+    fah = Fahrenheit()
+
+temp = Temperature()
+temp.cel = 30
+print(temp.fah)     # 86.0
+temp.fah = 100
+print(temp.cel)     # 37.77777777777778
