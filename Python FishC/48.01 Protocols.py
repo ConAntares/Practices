3888964225d3a9caeb3d5b976760441de6bceb77
ConAntares <LukeNiu@outlook.com>
2019-05-06 04:40:00 +0800

    update

--- Python FishC/48.01 Protocols.py
+++ Python FishC/48.01 Protocols.py
@@ -14,3 +14,19 @@ __setitem__()
 __delitem__()
 """
 
+# A Immutable List
+class CountList():
+    def __init__(self, *args):
+        self.values = [x for x in args]
+        self.count  = {}.fromkeys(range(len(self.values)),0)
+    def __len__(self):
+        return len(self.values)
+    def __getitem__(self,key):
+        self.count[key] += 1
+        return self.values[key]
+
+c1 = CountList(1,3,5,7,9)
+c1[3]
+c2 = CountList(0,2,4,6,8)
+print(c1.count)
+    # {0: 0, 1: 0, 2: 0, 3: 1, 4: 0}
