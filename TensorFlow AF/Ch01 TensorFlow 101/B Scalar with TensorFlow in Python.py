3c3e70d29a6f74a4a9783f6e72b5d2e1ee201ed7
ConAntares <LukeNiu@outlook.com>
2019-05-12 04:14:45 +0800

    Update

--- TensorFlow AF/Ch01 TensorFlow 101/B Scalar with TensorFlow in Python.py
+++ TensorFlow AF/Ch01 TensorFlow 101/B Scalar with TensorFlow in Python.py
@@ -2,10 +2,20 @@
 
 import tensorflow as tf
 
+tfs = tf.InteractiveSession()
+
+## Constant
+
 c1 = tf.constant(5,   name="x")
 c2 = tf.constant(6.0, name="y")
 c3 = tf.constant(7.0, tf.float64, name="z")
+print("c1 (x):",c1)
+    # c1 (x): Tensor("x:0", shape=(), dtype=int32)
+print("c2 (y):",c2)
+    # c2 (y): Tensor("y:0", shape=(), dtype=float32)
+print("c3 (z):",c3)
+    # c3 (z): Tensor("z:0", shape=(), dtype=float64)
 
-print("c1 (x):",c1) # c1 (x): Tensor("x:0", shape=(), dtype=int32)
-print("c2 (y):",c2) # c2 (y): Tensor("y:0", shape=(), dtype=float32)
-print("c3 (z):",c3) # c3 (z): Tensor("z:0", shape=(), dtype=float64)
+run = tfs.run([c1,c2,c3])
+print("run([c1,c2,c3]):",run)
+    # run([c1,c2,c3]): [5, 6.0, 7.0]
