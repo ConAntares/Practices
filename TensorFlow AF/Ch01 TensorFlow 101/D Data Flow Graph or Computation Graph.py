da4e945d500c36db1f596a6e0226abceb1bf5f54
ConAntares <LukeNiu@outlook.com>
2019-05-15 03:57:03 +0800

    Update

---
+++ TensorFlow AF/Ch01 TensorFlow 101/D Data Flow Graph or Computation Graph.py
@@ -0,0 +1,27 @@
+#### Data Flow Graph or Computation Graph
+
+import tensorflow as tf
+
+tfs = tf.InteractiveSession()
+
+## We can get explicit access to the default graph using the following command:
+"""
+graph = tf.get_default_graph()
+"""
+
+## For example y = x1 + x2 + x3
+"""
+tf.add(x1 + x2 + x3)
+"""
+
+## Calculate Linear Model y = w×x + b
+w = tf.Variable([0.3], tf.float32)          # Define model parameter: w
+b = tf.Variable([-.3], tf.float32)          # Define model parameter: b
+x = tf.placeholder(tf.float32)              # Define model input and output
+y = w * x + b
+output = 0
+with tf.Session() as tfs:
+    tf.global_variables_initializer().run() # Initialize
+    output = tfs.run(y, {x:[1,2,3,4]})      # Print the variable y
+print('output : ',output)
+    # output :  [0.         0.3        0.6        0.90000004]
