27404f80a04bcd51e3852e93c6382738fa2d9cc6
ConAntares <LukeNiu@outlook.com>
2019-05-15 02:52:09 +0800

    Update

--- TensorFlow AF/Ch01 TensorFlow 101/C Tensor with TensorFlow in Python.py
+++ TensorFlow AF/Ch01 TensorFlow 101/C Tensor with TensorFlow in Python.py
@@ -1,1 +1,24 @@
 #### Tensor with TensorFlow in Python
+
+import numpy as np
+import tensorflow as tf
+
+tfs = tf.InteractiveSession()
+
+## Create Tensor
+"""
+tf.convert_to_tensor(value, dtype=None, name=None, preferrred_dtype=None)
+"""
+
+# 0-Dimension Tensor
+T0 = tf.convert_to_tensor(5.0, dtype=tf.float64)
+re = tfs.run(T0)
+print(T0)                       # Tensor("Const:0", shape=(), dtype=float64)
+print(re)                       # 5.0
+
+# 1-Dimension Tensor
+A1 = np.array([1.0,2.0,3.0,4.0,])
+print(A1.shape)                 # (4,)
+print(A1.dtype)                 # float64
+T1 = tf.convert_to_tensor(A1, dtype=tf.float64)
+
