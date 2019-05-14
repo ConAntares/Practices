4107bbe97e366244f3e3026532ef153e0ffb05ad
ConAntares <LukeNiu@outlook.com>
2019-05-15 01:47:45 +0800

    Update

--- Gnuplot/Test_Gaston.jl
+++ Gnuplot/Test_Gaston.jl
@@ -4,8 +4,5 @@ using Gaston
 
 t = 0:0.01:1
 
-Gaston.set(print_fontface = "CMU-Serif")
-Gaston.plot(t, sin.(2π*5*t), title = "A sine wave")
-
-# got unsupported keyword argument "font"
-# got unsupported keyword argument "print_fonface"
+set(font = "Serif")
+plot(t,sin.(2π*5*t), title = "A sine wave")
